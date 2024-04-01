bcf75d74e3bd0367adb12deabfb6e738454d04fb
ntsafas <ntsafas@gmail.com>
2024-04-01 21:17:51 +0300

    Refactor Result structs

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,18 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = 
+    match TcpListener::bind("127.0.0.1:7878") {
+        Ok(t) => t,
+        Err(e) => panic!("Fail message: {}", e),
+    };
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let stream =
+        match stream {
+            Ok(t) => t,
+            Err(e) => panic!("Fail message: {}", e),
+        };
 
         handle_connection(stream);
     }
@@ -16,8 +24,10 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     //Read TcpStream
     let mut buffer: [u8; 1024] = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    match stream.read(&mut buffer) {
+        Ok(_n) => println!("Request: {}", String::from_utf8_lossy(&buffer[..])),
+        Err(e) => panic!("Fail message: {}", e),
+    };
 
     //Parse request header
     let get = b"GET / HTTP/1.1\r\n";
@@ -28,7 +38,11 @@ fn handle_connection(mut stream: TcpStream) {
     };
 
     //Create response string
-    let contents = fs::read_to_string(filename).unwrap();
+    let contents = 
+    match fs::read_to_string(filename) {
+        Ok(t) => t,
+        Err(e) => panic!("Fail message: {}", e),
+    };
     let response = format!(
         "{}\r\nContent-Length: {}\r\n\r\n{}",
         status_line,
@@ -37,6 +51,12 @@ fn handle_connection(mut stream: TcpStream) {
     );
 
     //Write response to client
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    match stream.write_all(response.as_bytes()) {
+        Ok(_t) => (),
+        Err(e) => panic!("Fail message: {}", e),
+    }
+    match stream.flush() {
+        Ok(_t) => (),
+        Err(e) => panic!("Fail message: {}", e),
+    }
 }
